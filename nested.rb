6c00774e0fc375c47017385386f5606258c3864e
David Cantoran <davidcantoran36@gmail.com>
2019-03-22 03:34:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,26 +81,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     
-     programmer_hash.replace({
-        :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-        },
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     })
-     programmer_hash
 }
     
 end
