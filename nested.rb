bf589bdd685e859d51baf6ba31c2d82d17a12b1a
David Cantoran <davidcantoran36@gmail.com>
2019-03-22 03:05:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,8 +65,9 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-  yukihiro_matsumoto => {
-    
+  response = :yukihiro_matsumoto => {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
   }
 
 	programmer_hash = 
